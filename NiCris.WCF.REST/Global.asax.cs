7f9ca9cb623e992b11599c7e57000a62b363b49c
BorisMomtchev <boris.momtchev@gmail.com>
2012-08-26 10:59:52 -0500

    First commit for VS 2012

---
+++ NiCris.WCF.REST/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceModel.Activation;
+using System.Web;
+using System.Web.Routing;
+
+namespace NiCris.WCF.REST
+{
+    public class Global : HttpApplication
+    {
+        private void RegisterRoutes()
+        {
+            // Non Security
+            RouteTable.Routes.Add(new ServiceRoute("BizMsgService", new WebServiceHostFactory2(), typeof(BizMsgService)));
+            
+            // Win Auth
+            RouteTable.Routes.Add(new ServiceRoute("BizMsgServiceWinAuth", new WebServiceHostFactory2(), typeof(BizMsgServiceWinAuth)));
+
+            // Basic Auth thru Interceptor
+            RouteTable.Routes.Add(new ServiceRoute("BizMsgServiceBasicAuth", new WebServiceHostFactory2BasicAuth(), typeof(BizMsgServiceBasicAuth)));
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes();
+        }
+    }
+}
