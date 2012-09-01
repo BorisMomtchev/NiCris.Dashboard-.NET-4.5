b7e594f9d88471dba46650af227779edb971d235
BorisMomtchev@g.com <BorisMomtchev@g.com>
2012-08-31 23:08:46 -0500

    Some Integ Tests

--- NiCris.IntegTests/Core/BizMsgExTests.cs
+++ NiCris.IntegTests/Core/BizMsgExTests.cs
@@ -44,5 +44,50 @@ namespace NiCris.IntegTests.Core
             foreach (var item in result)
                 repository.Insert(item);
         }
+
+        [Test]
+        public void BizMsgEx_Delete()
+        {
+            // Arrange -> Act -> Assert
+            var repository = GetRepository();
+
+            var all = repository.GetAll();
+
+            foreach (var item in all)
+                repository.Delete(item);
+        }
+
+        [Test]
+        public void BizMsg_CRUD_Test()
+        {
+            // Arrange -> Act -> Assert
+            var repository = GetRepository();
+
+            var newBizMsgEx = new BizMsgEx
+            {
+                // All required fields have valid values here...
+                EntityName = "EntityName:XYZ",
+                EntityAction = "EntityAction:ABC",
+                EntityValue = "EntityValue:123",
+                EntityType = "EntityType",
+                Date = DateTime.Now,
+                User = "User:XYZ",
+            };
+
+            // save to db
+            int id = repository.Insert(newBizMsgEx);
+
+            // get from db
+            var savedBizMsgEx = repository.GetById(id);
+            Assert.AreEqual("EntityValue:123", savedBizMsgEx.EntityValue);
+
+            // update
+            savedBizMsgEx.EntityValue = "UPDATED_EntityValue:123";
+            int id_updated = repository.Update(savedBizMsgEx);
+            Assert.AreEqual(id, id_updated);
+
+            // delete
+            repository.Delete(savedBizMsgEx);
+        }
     }
 }
--- NiCris.Web/readme.txt
+++ NiCris.Web/readme.txt
@@ -4,10 +4,12 @@
 
 HTTP GET - Gets all BizMsgs
 http://localhost:36637/api/BizMsgService/
+http://localhost:36637/api/BizMsgExService/
 
 ####################################################
 
 HTTP GET - Gets a Single BizMsg
 http://localhost:36637/api/BizMsgService/{id}
+http://localhost:36637/api/BizMsgExService/{id}
 
 ####################################################
